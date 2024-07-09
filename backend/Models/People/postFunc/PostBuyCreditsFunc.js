f75015d6703b13fdf50bd37c8ac2d0bc053754e1
Bhaskar-459 <m03bhaskar@gmail.com>
2024-07-09 11:51:18 +0530

    completed the transactions part

--- backend/Models/People/postFunc/PostBuyCreditsFunc.js
+++ backend/Models/People/postFunc/PostBuyCreditsFunc.js
@@ -1,26 +1,35 @@
 import people from "../../../Database/Schemas/People.js";
-import createTransaction from "../../Transactions/Transaction.js";
+import createTransaction from "../../Transactions/createTransaction.js";
 import updateTransactionHistoryForPeople from "../../Transactions/UpdateTransactionHistoryForPeople.js";
 
 const postBuyCreditsFunc = async (req, res) => {
     const { email, credits } = req.body;
     try {
         const person = await people.findOne({ email });
+        if (!person) {
+            return res.status(404).json({ message: 'Person not found' });
+        }
+        
         person.portfolio.currentCredits += credits;
         await person.save();
+        
         const TransactionObj = {
             TransactionId: 1,
             category: "People",
-            PersonName: person.Name,
-            creditValue: credits*10,
+            TransactionDate: new Date(),
+            TransactionType: "Buy",
+            PersonName: person._id,
+            creditValue: credits * 10,
             NoOfCredits: credits
-        }
-        await createTransaction({TransactionObj});
-        await updateTransactionHistoryForPeople({TransactionObj}, person, "Buy");
+        };
+
+        await createTransaction({ TransactionObj });
+        await updateTransactionHistoryForPeople({ TransactionObj }, person, "Buy");
         res.json(person);
-    }
-    catch (error) {
+    } catch (error) {
+        console.error(error);
         return res.status(500).json({ message: error.message });
     }
-}
+};
+
 export default postBuyCreditsFunc;
--- backend/Models/Transactions/createTransaction.js
+++ backend/Models/Transactions/createTransaction.js
@@ -1,20 +1,23 @@
-import TransactionsModel  from "../../Database/Schemas/Transactions.js";
+import TransactionsModel from "../../Database/Schemas/Transactions.js";
 
-const createTransaction = async ({TransactionObj}) => {
+const createTransaction = async ({ TransactionObj }) => {
     try {
         const Transaction = new TransactionsModel({
             TransactionId: TransactionObj.TransactionId,
             category: TransactionObj.category,
             PersonName: TransactionObj.PersonName,
             creditValue: TransactionObj.creditValue,
-            NoOfCredits: TransactionObj.NoOfCredits
+            NoOfCredits: TransactionObj.NoOfCredits,
+            TransactionDate: TransactionObj.TransactionDate,
+            TransactionType: TransactionObj.TransactionType
         });
         const savedTransaction = await Transaction.save();
+
         return savedTransaction;
-    }
-    catch (error) {
+    } catch (error) {
+        console.error(error);
         return error;
     }
-}
+};
 
 export default createTransaction;
