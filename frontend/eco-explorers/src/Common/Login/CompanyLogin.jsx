22e44e7e3c780acfbe2e0b0653d2078db9fb233d
HarshaVardhan3112 <153989904+HarshaVardhan3112@users.noreply.github.com>
2024-07-13 21:37:59 +0530

    ngo, company, user login page

--- frontend/eco-explorers/src/Common/Login/CompanyLogin.jsx
+++ frontend/eco-explorers/src/Common/Login/CompanyLogin.jsx
@@ -9,6 +9,7 @@ function LoginCompany() {
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -20,9 +21,12 @@ function LoginCompany() {
       if (response.status === 200) {
         //Handle successful login, e.g., store user data, redirect, etc.
         alert('Login successful');
-      } else if(response.status === 404){
+        localStorage.setItem("password", password);
+
+        window.location.href = '/company';
+      } else if (response.status === 404) {
         // Handle failed login
-        alert('Invalid credentials'); 
+        alert('Invalid credentials');
       }
     } catch (error) {
       // Handle errors
@@ -32,41 +36,41 @@ function LoginCompany() {
 
   return (
     <div className=" LoginContainer">
-    <div className="login-box">
-      <h3>Welcome to..</h3>
-      <h1>Green Trade Exchange</h1>
-      <h2><span className='L_login'>L</span>ogin</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder='Enter your email address'
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder="Enter your password"
-          />
-          <Link to="/forgotPassword" className="forgot-password">Forgot Password</Link>
-        </div>
+      <div className="login-box">
+        <h3>Welcome to..</h3>
+        <h1>Green Trade Exchange</h1>
+        <h2><span className='L_login'>L</span>ogin</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder='Enter your email address'
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Enter your password"
+            />
+            <Link to="/forgotPassword" className="forgot-password">Forgot Password</Link>
+          </div>
           <button type="submit" disabled={loading || !email || !password}>
             {loading ? 'Logging in...' : 'Login'}
           </button>
-      </form>
+        </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
-      <div className="register-link">
-        No Account? <Link to="/registerCompany">Sign up</Link>
+        <div className="register-link">
+          No Account? <Link to="/registerCompany">Sign up</Link>
+        </div>
       </div>
-    </div>
     </div>
 
   );
--- frontend/eco-explorers/src/Common/Login/PeopleLogin.jsx
+++ frontend/eco-explorers/src/Common/Login/PeopleLogin.jsx
@@ -2,10 +2,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './login.css';
 
 function LoginPeople() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -18,9 +21,12 @@ function LoginPeople() {
       if (response.status === 200) {
         //Handle successful login, e.g., store user data, redirect, etc.
         alert('Login successful');
-      } else if(response.status === 404){
+        localStorage.setItem("password", password);
+        window.location.href = '/company';
+        
+      } else if (response.status === 404) {
         // Handle failed login
-        alert('Invalid credentials'); 
+        alert('Invalid credentials');
       }
     } catch (error) {
       // Handle errors
@@ -29,30 +35,42 @@ function LoginPeople() {
   };
 
   return (
-    <div className='LoginContainer'>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className=" LoginContainer">
+      <div className="login-box">
+        <h3>Welcome to..</h3>
+        <h1>Green Trade Exchange</h1>
+        <h2><span className='L_login'>L</span>ogin</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder='Enter your email address'
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Enter your password"
+            />
+            <Link to="/forgotPassword" className="forgot-password">Forgot Password</Link>
+          </div>
+          <button type="submit" disabled={loading || !email || !password}>
+            {loading ? 'Logging in...' : 'Login'}
+          </button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <div className="register-link">
+          No Account? <Link to="/registerPeople">Register</Link>
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <Link to="/registerPeople">Register</Link>
+      </div>
     </div>
 
   );
