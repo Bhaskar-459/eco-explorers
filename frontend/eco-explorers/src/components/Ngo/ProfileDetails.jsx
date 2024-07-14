bf3d17b3bb1fe1b3d6e365f30d4026c5d2f6619e
AetherGb <95398090+AetherGb@users.noreply.github.com>
2024-07-14 12:59:21 +0530

    1407bySai

--- frontend/eco-explorers/src/components/Ngo/ProfileDetails.jsx
+++ frontend/eco-explorers/src/components/Ngo/ProfileDetails.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, {useState } from 'react'
 
 
-const ProfileDetails = (props) => {
-  const response = await axios.get('http://localhost:5000/api/ngo/get/getProfile');
+
+
+
+const ProfileDetails =(props) =>  {
+
+  let userDetails=JSON.parse(localStorage.getItem("userDetails"));
+  
+  
+
+  console.log(userDetails);
   return (
+    
     <div className='profileDetailsContainer'>
       <div className='profileImage'>
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
       </div>
       <div className='listDetailsContainer'>
         <div className='personalProfileDetails'>
-          <h2>NGO Name</h2>
-          <p>Location</p>
-          <p>Phone Number</p>
-          <p>Email</p>
+          <h2>{userDetails.personalInfo.name}</h2>
+          <p>{userDetails.personalInfo.phone}</p>
+          <p>{userDetails.personalInfo.address}</p>
+          <p>{userDetails.email}</p>
         </div>
         <div className='financialProfileDetails'>
           <div className='noofCredits'></div>
