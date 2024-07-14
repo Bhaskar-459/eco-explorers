00198d452d1a1962ded1341b79a45d69cebd41f1
AetherGb <95398090+AetherGb@users.noreply.github.com>
2024-07-14 11:22:09 +0530

    1407Sai

--- frontend/eco-explorers/src/components/Ngo/ProfileDetails.jsx
+++ frontend/eco-explorers/src/components/Ngo/ProfileDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const ProfileDetails = props => {
+
+const ProfileDetails = (props) => {
+  const response = await axios.get('http://localhost:5000/api/ngo/get/getProfile');
   return (
-    <div>ProfileDetails</div>
+    <div className='profileDetailsContainer'>
+      <div className='profileImage'>
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+      </div>
+      <div className='listDetailsContainer'>
+        <div className='personalProfileDetails'>
+          <h2>NGO Name</h2>
+          <p>Location</p>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </div>
+        <div className='financialProfileDetails'>
+          <div className='noofCredits'></div>
+          <div className='gcpDetails'></div>
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
