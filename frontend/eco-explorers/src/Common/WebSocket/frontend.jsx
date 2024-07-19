c55b4732108aa561d1a6ec6b2d1a37ce6f41c4db
AetherGb <95398090+AetherGb@users.noreply.github.com>
2024-07-19 17:43:52 +0530

    frontendsocket

--- frontend/eco-explorers/src/Common/WebSocket/frontend.jsx
+++ frontend/eco-explorers/src/Common/WebSocket/frontend.jsx
@@ -1,50 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 // import './App.css'
 import { io } from 'socket.io-client'
 
 const Frontend = () => {
-
-  const value = useState('hi');
+  const [value, setValue] = useState('hi');
   const [data, setData] = useState([]);
-  const [message, setMessage] = useState();
-  
-  const socket = io('http://localhost:4001')
+  const [message, setMessage] = useState('');
+
+  const socket = useRef(null);
 
   useEffect(() => {
-    socket.on('connect', () =>{
-      console.log('connected ', socket.id)
-      
-    })
+    socket.current = io('http://localhost:4001');
 
-    // socket.on('welcome', (msg) => console.log(msg))
+    socket.current.on('connect', () => {
+      console.log('connected ', socket.current.id)
+    });
 
-    socket.on('received_msg', (msg) => {
+    socket.current.on('received_msg', (msg) => {
       console.log(`received msg is ${msg}`);
-      setMessage(msg)
-      setData(prevData => [...prevData, message])
-     
-    })
+      setMessage(msg);
+      setData(prevData => [...prevData, msg]);
+    });
 
     return () => {
-      socket.disconnect()
+      socket.current.disconnect();
     }
-  },[])
+  }, []);
 
-  const handleClick = () =>{
-    
-    
-    socket.emit('message', value)
-    setData(prevData => [...prevData, message])
-    
+  const handleClick = () => {
+    socket.current.emit('message', value);
+    setData(prevData => [...prevData, value]);
   }
+
   const handleChange = (e) => {
-    setValue(e.target.value)
+    setValue(e.target.value);
   }
 
   return (
-    <div style={{margin:"auto"}}>
-      <div className="msg-box" style={{width:"100vw", height:"10rem"}}>{data.map((item, index) => <h1 key={index}>{item}</h1>)}</div>
-      <input onChange={handleChange} type="text" id="msg" />
+    <div style={{ margin: "auto" }}>
+      <div className="msg-box" style={{ width: "100vw", height: "10rem" }}>
+        {data.map((item, index) => <h1 key={index}>{item}</h1>)}
+      </div>
+      <input onChange={handleChange} type="text" id="msg" value={value} />
       <button onClick={handleClick}>Send</button>
     </div>
   )
